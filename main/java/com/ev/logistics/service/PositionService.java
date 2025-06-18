4dbf496b7c383861b539138b91a6e1a1bf88f1c8
shujing55555 <1621729798@qq.com>
2025-06-18 08:26:50 +0800

    Add files via upload

---
+++ main/java/com/ev/logistics/service/PositionService.java
@@ -0,0 +1,7 @@
+package com.ev.logistics.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.ev.logistics.entity.Position;
+
+public interface PositionService extends IService<Position> {
+}
