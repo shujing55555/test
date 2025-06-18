cec80c54912aab6fd31869a57f10f282dd93c330
shujing55555 <1621729798@qq.com>
2025-06-18 08:25:12 +0800

    Delete src directory

--- src/main/java/com/ev/logistics/dao/PositionDao.java
+++
@@ -1,9 +0,0 @@
-package com.ev.logistics.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ev.logistics.entity.Position;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PositionDao extends BaseMapper<Position> {
-}
