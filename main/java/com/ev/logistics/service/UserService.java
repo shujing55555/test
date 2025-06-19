1c53552dd2ee7e33854a3c575b014e53d930fa12
shujing55555 <1621729798@qq.com>
2025-06-19 11:21:19 +0800

    Delete main directory

--- main/java/com/ev/logistics/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.ev.logistics.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.ev.logistics.entity.User;
-
-
-public interface UserService extends IService<User> {
-}
