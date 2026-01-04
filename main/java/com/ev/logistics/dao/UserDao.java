c4d46c18275f620aff7542b9fe7f77ab9c138164
shujing55555 <1621729798@qq.com>
2026-01-04 15:57:37 +0800

    Delete main directory

--- main/java/com/ev/logistics/dao/UserDao.java
+++
@@ -1,8 +0,0 @@
-package com.ev.logistics.dao;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ev.logistics.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-@Mapper
-public interface UserDao extends BaseMapper<User> {
-}
--- main/java/com/ev/logistics/service/CategoryService.java
+++
@@ -1,8 +0,0 @@
-package com.ev.logistics.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.ev.logistics.entity.Category;
-
-
-public interface CategoryService extends IService<Category> {
-}
